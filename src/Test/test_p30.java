81fd4355d1ad0863f51e229d5293911d02466516
Laixuzhen123 <a33909121@gmail.com>
2021-11-03 15:25:33 +0800

    first

---
+++ src/Test/test_p30.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class test_p30 {
+
+	public static void main(String[] args) {
+		int i,j;
+		for(i = 0;i < 5;i++) {
+			for(j = 0;j < 3;j++) {
+				System.out.println("i°é"+i+":j¬O"+j);
+			}
+		}
+
+	}
+
+}
